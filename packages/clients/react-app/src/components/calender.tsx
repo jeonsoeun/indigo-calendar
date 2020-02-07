0e636dc7935c875f2792ba95f90f1563cc39c0a1
Soeun Jun <jse525@neowiz.com>
2020-02-07 11:10:36 +0900

    remove console log

--- packages/clients/react-app/src/components/calender.tsx
+++ packages/clients/react-app/src/components/calender.tsx
@@ -1,71 +1,20 @@
 import React from 'react'
+import { RootState } from '../store'
+import { useSelector } from 'react-redux'
+
 import '../styles/components/calender.scss'
 import CalendarHead from '../components/CalenderHead'
-import CalendarCell from '../components/CalendarCell'
+import CalendarBody from '../components/CalendarBody'
 
-interface IProp {
-  year: number
-  month: number
-  day: number
-  selectedDate: Date
-  today: Date
-}
-
-export const Calendar: React.FC<IProp> = (props) => {
-  const week = ['Sun', 'Mon', 'Tue', 'Wen', 'Thu', 'Fri', 'Sat']
-
-  const makeTableCell = (curDate: Date) => {
-    const dateTable = []
-    for (let d = 0; d < 7; d++) {
-      const curDay = curDate.getDate()
-      const curMonth = curDate.getMonth() + 1
-      dateTable.push(
-        <CalendarCell
-          curMonth={curMonth}
-          curDay={curDay}
-          isIncludedDay={curMonth === props.month}
-          isSelected={props.selectedDate === curDate}
-          key={curDay + 10 * curMonth}
-        ></CalendarCell>
-      )
-      curDate.setDate(curDate.getDate() + 1)
-    }
-    return dateTable
-  }
-
-  const weekTable = []
-  const curDate = new Date(props.year, props.month - 1, 1)
-  curDate.setDate(-curDate.getDay())
-  curDate.setDate(curDate.getDate() + 1)
-  for (let w = 0; w < 6; w++) {
-    weekTable.push(
-      <tr className="cb-tr" key={w}>
-        {makeTableCell(curDate)}
-      </tr>
-    )
-  }
+export const Calendar: React.FC<{}> = () => {
+  const { year, month, day } = useSelector((state: RootState) => ({
+    ...state.calendar,
+  }))
 
   return (
     <div className="Calendar">
-      <CalendarHead
-        year={props.year}
-        month={props.month}
-        day={props.day}
-      ></CalendarHead>
-      <table>
-        <thead>
-          <tr>
-            {week.map((w, i) => {
-              return (
-                <th className="ch-tr" key={i}>
-                  {w}
-                </th>
-              )
-            })}
-          </tr>
-        </thead>
-        <tbody>{weekTable}</tbody>
-      </table>
+      <CalendarHead year={year} month={month} day={day}></CalendarHead>
+      <CalendarBody></CalendarBody>
     </div>
   )
 }
--- packages/clients/react-app/src/index.tsx
+++ packages/clients/react-app/src/index.tsx
@@ -9,7 +9,6 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 
 const store = createStore(rootReducer)
-console.log(store)
 
 ReactDOM.render(
   <Provider store={store}>
